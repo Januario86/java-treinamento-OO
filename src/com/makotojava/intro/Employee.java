4b2f6d85bc251cb38fa81d9551968e3bd33018e0
januario86 <januario86fp@gmail.com>
2020-06-22 16:53:16 -0300

    Application main

---
+++ src/com/makotojava/intro/Employee.java
@@ -0,0 +1,15 @@
+package com.makotojava.intro;
+
+public class Employee extends Person {
+
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Employee(String name, int age, int height, int weight, String eyeColor, String gender) {
+		super(name, age, height, weight, eyeColor, gender);
+		// TODO Auto-generated constructor stub
+	}
+
+}
